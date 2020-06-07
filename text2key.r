2257b9e9d92e6644f0d8d6de80879db99d5e9573
iarwain <iarwain@orx-project.org>
2020-06-07 07:49:42 -0400

    - Added functional autohotkey backend (WIP, needs some cleanup)

--- text2key.r
+++ text2key.r
@@ -5,7 +5,55 @@ REBOL [
   file: %text2key.r
 ]
 
+backend: context [
+  file: none buffer: copy []
+  emit: func [data] [
+    repend buffer data
+  ]
+  save: does [
+    write/lines file buffer
+  ]
+  new: func [file'] [
+    make self [
+      file: to-rebol-file file'
+    ]
+  ]
+]
+
+autohotkey: make backend [
+  emit {SetKeyDelay, 40, 0}
+  end: true
+  move: func [delta] [
+    emit rejoin [{Send ^{} pick [{Up} {Down}] delta < 0 { } abs delta {^}}]
+    end: (current + delta) > length? result
+  ]
+  delay: func [duration] [
+    emit rejoin [{Sleep } to-integer 1000 * load duration]
+  ]
+  copy: func [source target count] [
+    ; TODO
+  ]
+  insert: func [lines /local carry text] [
+    if carry: to-logic all [not end not empty? lines not empty? result/:current] [
+      emit {SendEvent {Text}`n}
+      emit {Send {Up}}
+    ]
+    forall lines [
+      text: replace/all system/words/copy lines/1 {;} {`;}
+      emit rejoin [{SendEvent {Text}} text pick [{} {`n}] carry and last? lines]
+    ]
+    if carry [
+      emit {Send {Right}}
+    ]
+  ]
+  remove: func [count] [
+    emit rejoin [{Send {Shift Down}^{Down } count {^}{Shift Up}{Delete}}]
+  ]
+]
+
 if attempt [exists? file: to-file system/options/args/1] [
+
+  output: autohotkey/new join file %.ahk
 
   use [current section action target] [
     label: [integer! opt ["." integer!]]
@@ -30,14 +78,14 @@ if attempt [exists? file: to-file system/options/args/1] [
     parse read file [
       any [
         section-marker (current: add-section section action target)
-      | copy line thru lf (append current/content line current/line-count: current/line-count + 1)
+      | copy line thru lf (append current/content trim/tail line current/line-count: current/line-count + 1)
       ]
     ]
   ]
 
   steps: sort/compare extract sections 2 func [a b] [(load a) < (load b)]
   either steps = unique steps [
-    use [section result replaced target current] [
+    use [section replaced target] [
       find-line: funct [section /with current] [
         result: 1
         foreach [label content] sections [
@@ -58,6 +106,7 @@ if attempt [exists? file: to-file system/options/args/1] [
       move-to: func [line] [
         if current != line [
           print [{  . Moving to line} line rejoin [{[} pick [{+} {}] line > current line - current {]}]]
+          output/move line - current
           current: line
         ]
       ]
@@ -68,6 +117,7 @@ if attempt [exists? file: to-file system/options/args/1] [
         switch section/action [
           delay [
             print [{  . Delaying} load section/target {seconds}]
+            output/delay section/target
           ]
           copy [
             target: find-line/with section/target step
@@ -78,6 +128,7 @@ if attempt [exists? file: to-file system/options/args/1] [
               move-to target: find-line/with section/target step
               print [{  . Replacing [} section/target {], removing} sections/(section/target)/line-count {lines at line} target]
               remove/part at result target sections/(section/target)/line-count
+              output/remove sections/(section/target)/line-count
               append replaced section/target
             ] [
               print [{!! Aborting: trying to replace [} section/target {] which was already replaced earlier}]
@@ -88,9 +139,11 @@ if attempt [exists? file: to-file system/options/args/1] [
         move-to find-line step
         print [{  . Inserting} section/line-count {lines}]
         insert at result find-line step section/content
+        output/insert section/content
         current: current + section/line-count
       ]
-      foreach l result [print trim/tail l]
+      output/save
+      ;foreach l result [print trim/tail l]
     ]
   ] [
     print [{!! Can't process, duplicated steps found in [} steps {]}]
